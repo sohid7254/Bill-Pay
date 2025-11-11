97a220f726780e68bd182dcaf7bdfd600513c7a0
sohid7254 <sohidameen321@gmail.com>
2025-11-11 19:04:38 +0600

    Added Footer with others useful links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,42 +1,123 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo.png"
+import { SlSocialFacebook } from "react-icons/sl";
+import { GrLinkedinOption } from "react-icons/gr";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+import { FiMapPin } from "react-icons/fi";
+import { IoIosMailOpen } from "react-icons/io";
+import { FaPhoneAlt } from "react-icons/fa";
 
+// Footer component
 const Footer = () => {
+    
+    
+
+    
+    const headingClass = "text-lg font-semibold mb-4 border-b-2 border-b-current pb-2 w-2/4 sm:w-1/3 md:w-1/2 lg:w-3/4  text-gray-100";
+
+    
+    const linkClass = "text-gray-200 hover:text-[#8559ff] transition duration-200 cursor-pointer text-sm mb-2 flex items-center";
+
     return (
-        <div className="bg-base-200">
-            <footer className="max-w-7xl mx-auto footer sm:footer-horizontal p-4">
-                <aside>
-                    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current">
-                        <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-                    </svg>
-                    <p>
-                        ACME Industries Ltd.
-                        <br />
-                        Providing reliable tech since 1992
-                    </p>
-                </aside>
-                <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </nav>
-            </footer>
-        </div>
+        <footer className="bg-gray-700 text-gray-300 font-sans mt-12 pt-10 ">
+            <div className="max-w-7xl mx-auto px-4 sm:px-4 lg:px-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 lg:gap-8 pb-2">
+                    <div className="lg:col-span-1">
+                        <div className="flex flex-col space-x-1 mb-4">
+                            <img src={logo} alt="logo" className="w-25" />
+                            <p>Bill Pay is a smart solution for bill payment and financial management. We are committed to making your transactions easy, secure, and timely.</p>
+                        </div>
+                        <p className="text-sm text-gray-400 leading-relaxed max-w-sm"></p>
+                    </div>
+
+                    <div>
+                        <h4 className={headingClass}>Quick Links</h4>
+                        <ul className="space-y-2">
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    About
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4 className={headingClass}>Company Policy</h4>
+                        <ul className="space-y-2">
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    Terms and Conditions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" className={linkClass}>
+                                    Privacy Policy
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h4 className={headingClass}>Contact Us</h4>
+                        <ul className="space-y-3">
+                            <li className="text-gray-400 text-sm flex items-start">
+                                <FiMapPin size={18} className="mr-3 mt-1 text-gray-500" />
+
+                                <span>22 Banani,Bangladesh</span>
+                            </li>
+                            <li className="text-gray-400 text-sm flex items-start">
+                                <IoIosMailOpen size={18} className="mr-3 mt-1 text-gray-500" />
+
+                                <a href="mailto:demo@example.com" className="hover:text-[#8559ff] transition">
+                                    sohidameen321@gmail.com
+                                </a>
+                            </li>
+                            <li className="text-gray-400 text-sm flex items-start">
+                                <FaPhoneAlt size={18} className="mr-3 mt-1 text-gray-500" />
+                                
+                                <span>+8801637687254</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className={headingClass}>Social Links</h4>
+                        <div className="flex items-center space-x-3">
+                            <a href="#" className="flex items-center justify-center p-2 rounded-full bg-gray-800 hover:bg-[#8559ff] transition duration-300">
+                                <SlSocialFacebook size={20} />
+                            </a>
+                            <a href="#" className="flex items-center justify-center p-2 rounded-full bg-gray-800 hover:bg-[#8559ff] transition duration-300">
+                                <GrLinkedinOption size={20} />
+                            </a>
+                            <a href="#" className="flex items-center justify-center p-2 rounded-full bg-gray-800 hover:bg-[#8559ff] transition duration-300">
+                                <FaXTwitter size={20} />
+                            </a>
+                            <a href="#" className="flex items-center justify-center p-2 rounded-full bg-gray-800 hover:bg-[#8559ff] transition duration-300">
+                                <FaInstagram size={20} />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="border-t border-gray-300 pt-5 pb-5 text-xs text-center text-gray-200">
+                    <p>Copyright ©2025 Bill Pay. All Rights Reserved</p>
+                </div>
+            </div>
+        </footer>
     );
 };
-
 export default Footer;
