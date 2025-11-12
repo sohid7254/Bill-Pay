e51cb07c7f55432987124f592e5663c85ad668ae
sohid7254 <sohidameen321@gmail.com>
2025-11-12 21:08:01 +0600

    added recent section on home page as instructed

---
+++ src/Components/BillCard.jsx
@@ -0,0 +1,30 @@
+const BillCard = ({ bill }) => {
+    const { title, category, location, date, image, _id } = bill;
+
+    return (
+        <div className="bg-white rounded-lg shadow-md overflow-hidden border border-gray-100 flex flex-col">
+            {/* Image */}
+            <div className="p-4 pb-0">
+                <img src={image} alt={title} className="w-full h-40 object-cover rounded-lg" />
+            </div>
+
+            {/* Content */}
+            <div className="px-4 pt-4 pb-6 flex flex-col justify-between flex-grow">
+                <div>
+                    <h3 className="text-lg font-semibold text-[#8559ff] mb-1">{title}</h3>
+                    <p className="text-sm text-gray-600 mb-1">
+                        Category: <span className="bg-[#f3edff] border border-purple-200 px-2 rounded-lg text-sm font-semibold text-center text-[#8559ff]">{category}</span>
+                    </p>
+                    <p className="text-sm text-gray-600 mb-1">Location: {location}</p>
+                    <p className="text-sm text-gray-400 mb-3">Date: {date}</p>
+                </div>
+
+                <button onClick={() => console.log("See details for:", _id)} className="btn btn-outline btn-sm text-[#8559ff] border-[#8559ff] mt-4">
+                    See Details
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default BillCard;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import Banner from '../Components/Banner';
-import Category from '../Components/Category';
-import FeatureSection from '../Components/FeatureSection';
+import React, { useEffect, useState } from "react";
+import Banner from "../Components/Banner";
+import Category from "../Components/Category";
+import FeatureSection from "../Components/FeatureSection";
+import BillCard from "../Components/BillCard";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Home = () => {
+    const [latestBills, setLatestBills] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/latest-bills")
+            .then(res => res.json())
+            .then((data) => {
+                console.log("Latest bills", data);
+                setLatestBills(data);
+                
+            })
+            .catch((err) => console.log(err.message));
+    }, []);
     return (
         <div>
-            <Banner/>
-            <FeatureSection/>
-            <Category/>
+            <Banner />
+            <FeatureSection />
+            <Category />
+            <div>
+                <h2 className="md:text-3xl lg:4xl text-xl font-semibold text-center mt-5">Recent Bills</h2>
+
+                <div className=" mt-10 px-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                        {latestBills.map((bill) => (
+                            <BillCard key={bill._id} bill={bill}></BillCard>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div className="px-4 mt-6 flex justify-end">
+                <Link to={'/bills'} className="flex items-center gap-2 text-[#8559ff] font-semibold hover:underline">
+                    See More <FaLongArrowAltRight />
+                </Link>
+            </div>
         </div>
     );
 };
